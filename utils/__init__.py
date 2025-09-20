64e947ffc859a3aa8894c32fde8472c177ae7b3c
itzmarwin <itzm4rw1n@gmail.com>
2025-09-21 01:13:26 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,19 +3,28 @@ from .telegram_utils import (
     generate_pack_link,
     is_sticker_supported,
     extract_sticker_from_message,
+    extract_gif_from_message,
     is_private_chat,
     is_group_chat,
     validate_pack_name,
     format_pack_creation_message,
-    format_sticker_added_message
+    format_sticker_added_message,
+    format_gif_conversion_message
 )
 
 from .sticker_utils import (
     download_sticker,
+    download_gif,
     create_sticker_pack,
     add_sticker_to_pack,
     get_sticker_emoji,
     cleanup_temp_file
+)
+
+from .file_converters import (
+    convert_sticker_to_png,
+    convert_webm_to_mp4,
+    convert_gif_to_webm
 )
 
 __all__ = [
@@ -24,16 +33,24 @@ __all__ = [
     "generate_pack_link", 
     "is_sticker_supported",
     "extract_sticker_from_message",
+    "extract_gif_from_message",
     "is_private_chat",
     "is_group_chat",
     "validate_pack_name",
     "format_pack_creation_message",
     "format_sticker_added_message",
+    "format_gif_conversion_message",
     
     # Sticker utilities
     "download_sticker",
+    "download_gif",
     "create_sticker_pack",
     "add_sticker_to_pack",
     "get_sticker_emoji",
-    "cleanup_temp_file"
+    "cleanup_temp_file",
+    
+    # File converters
+    "convert_sticker_to_png",
+    "convert_webm_to_mp4",
+    "convert_gif_to_webm"
 ]

9e7860b7d39651fc0d0483a27d60bf72e4e35295
itzmarwin <itzm4rw1n@gmail.com>
2025-09-21 01:14:36 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,13 @@
 from .start import start_router
 from .kang import kang_router
+from .stickerid import stickerid_router
+from .getsticker import getsticker_router
+from .getvidsticker import getvidsticker_router
 
-__all__ = ["start_router", "kang_router"]
+__all__ = [
+    "start_router", 
+    "kang_router", 
+    "stickerid_router", 
+    "getsticker_router", 
+    "getvidsticker_router"
+]

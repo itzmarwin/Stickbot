91b8192c107d11a5c290383c79bd1bcc3e707f05
itzmarwin <itzm4rw1n@gmail.com>
2025-10-07 16:53:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ from pyrogram import Client
 
 from config import BOT_TOKEN, TELEGRAM_API_ID, TELEGRAM_API_HASH
 from database import init_db
-from handlers import start, kang, packs, misc, logger
+from handlers import start, kang, packs, misc, logger, gban  # NEW: gban added here
 from telethon_quotly import setup_telethon_handlers
 from pyrogram_handlers.afk import setup_afk_handlers
-from handlers.gban_handlers import router as gban_router  # Aiogram GBan handlers
 
 # Configure logging
 logging.basicConfig(
@@ -72,7 +71,7 @@ async def main():
         dp.include_router(packs.router)
         dp.include_router(logger.router)
         dp.include_router(misc.router)
-        dp.include_router(gban_router)  # Aiogram GBan handlers included
+        dp.include_router(gban.router)  # NEW: GBan router included
         
         # Initialize Telethon client for /q command
         global telethon_client

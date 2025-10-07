c4634c46886c6d765676b12148a72536a1ffae2e
itzmarwin <itzm4rw1n@gmail.com>
2025-10-07 17:05:18 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,15 @@
+from .start import router as start_router
+from .kang import router as kang_router  
+from .packs import router as packs_router
+from .misc import router as misc_router
+from .logger import router as logger_router
+from .gban import router as gban_router
 
+__all__ = [
+    "start_router",
+    "kang_router", 
+    "packs_router",
+    "misc_router",
+    "logger_router",
+    "gban_router",
+]

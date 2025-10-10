287b935d2d497434c8c0b0f1b1902424b9d089e7
itzmarwin <itzm4rw1n@gmail.com>
2025-10-10 18:42:31 +0530

    Update fsm_states.py

--- utils/fsm_states.py
+++ utils/fsm_states.py
@@ -3,3 +3,10 @@ from aiogram.fsm.state import State, StatesGroup
 class KangStates(StatesGroup):
     """FSM states for kang command"""
     waiting_for_pack_name = State()
+
+class PackManagementStates(StatesGroup):
+    """FSM states for pack management"""
+    waiting_for_new_pack_name = State()
+    waiting_for_rename_pack_name = State()
+    waiting_for_sticker_to_add = State()
+    confirming_delete_pack = State()

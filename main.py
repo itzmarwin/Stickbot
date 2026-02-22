10382e9f5af101db99f51ad8f022f238794f7b14
itzmarwin <itzm4rw1n@gmail.com>
2026-02-23 04:39:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,8 +66,8 @@ async def setup_pyrogram():
         await setup_welcome_handlers(pyro_client)
         await setup_goodbye_handlers(pyro_client)
         await setup_ban_handlers(pyro_client)
-        await setup_afk_handlers(pyro_client)
         await setup_filter_handlers(pyro_client)
+        await setup_afk_handlers(pyro_client)
         await setup_restart_handlers(pyro_client)
         await setup_extra_handlers(pyro_client)
 

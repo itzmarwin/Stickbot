552fa0c3e86f6d0cb9fe234d8d19a5cb10be4c06
itzmarwin <itzm4rw1n@gmail.com>
2026-02-23 04:27:35 +0530

    Update commands.py

--- pyrogram_handlers/commands.py
+++ pyrogram_handlers/commands.py
@@ -1,9 +1,25 @@
 from pyrogram import filters
 
-PREFIXES = ["!", ".", "/", "@"]
+PREFIXES = "/!.@"
 
 def cmd(commands):
-    return filters.command(commands, prefixes=PREFIXES)
+    if isinstance(commands, str):
+        commands = [commands]
+    
+    async def func(_, __, message):
+        text = message.text or message.caption or ""
+        if not text:
+            return False
+        
+        if not text[0] in PREFIXES:
+            return False
+        
+        command = text[1:].split()[0].lower()
+        command = command.split("@")[0]
+        
+        return command in [c.lower() for c in commands]
+    
+    return filters.create(func)
 
 def get_args(message) -> list:
     text = message.text or message.caption or ""

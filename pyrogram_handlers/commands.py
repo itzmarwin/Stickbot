fb33ec45bfdc4d463795dfaa2f57d0e33093bbb3
itzmarwin <itzm4rw1n@gmail.com>
2026-02-23 04:15:14 +0530

    Update commands.py

--- pyrogram_handlers/commands.py
+++ pyrogram_handlers/commands.py
@@ -4,3 +4,10 @@ PREFIXES = ["!", ".", "/", "@"]
 
 def cmd(commands):
     return filters.command(commands, prefixes=PREFIXES)
+
+def get_args(message) -> list:
+    text = message.text or message.caption or ""
+    parts = text.split()
+    if parts:
+        return parts[1:]
+    return []

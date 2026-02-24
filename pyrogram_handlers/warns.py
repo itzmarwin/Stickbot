c633e66ba2c4465bc8a08fa44139a305948e6a94
itzmarwin <itzm4rw1n@gmail.com>
2026-02-24 15:57:33 +0530

    Update warns.py

--- pyrogram_handlers/warns.py
+++ pyrogram_handlers/warns.py
@@ -135,7 +135,7 @@ def _action_text(user_mention: str, warn_count: int, warn_limit: int, admin_ment
     )
     if reason:
         text += f"\n<b>Reason:</b> {reason}"
-    text += f"\n\n⚠️ Warning limit reached! Applying action: <b>{mode}</b>."
+    text += f"\n\nWarning limit reached! Applying action: <b>{mode}</b>."
     return text
 
 

cd1beced1f33242d8f113fd09db66181f332f526
itzmarwin <itzm4rw1n@gmail.com>
2026-05-16 02:09:54 +0530

    Update filters.py

--- pyrogram_handlers/filters.py
+++ pyrogram_handlers/filters.py
@@ -228,8 +228,7 @@ async def setup_filter_handlers(client: Client):
             parse_mode=ParseMode.HTML
         )
 
-    # FIX: ~filters.me se bot apne messages pe trigger nahi karega
-    @client.on_message(filters.group & filters.text & ~filters.me, group=2)
+    @client.on_message(filters.group & filters.text & ~filters.me & ~filters.command, group=2)
     async def filter_trigger(client: Client, message: Message):
         chat_id = message.chat.id
 

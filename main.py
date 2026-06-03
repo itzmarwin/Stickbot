ab76033ad650ad41788c3ac6ebf4fffe1aa7efe4
itzmarwin <itzm4rw1n@gmail.com>
2026-06-03 23:31:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ from pyrogram_handlers.filters import setup_filter_handlers
 from pyrogram_handlers.warns import setup_warn_handlers
 from pyrogram_handlers.mute import setup_mute_handlers
 from pyrogram_handlers.locks import setup_locks_handlers
+from pyrogram_handlers.caching import setup_cache_handlers
 
 logging.basicConfig(
     level=logging.INFO,
@@ -121,6 +122,7 @@ async def setup_pyrogram():
         await setup_warn_handlers(pyro_client)
         await setup_mute_handlers(pyro_client)
         await setup_locks_handlers(pyro_client)
+        await setup_cache_handlers(pyro_client)
         await setup_filter_handlers(pyro_client)
         await setup_afk_handlers(pyro_client)
         await setup_restart_handlers(pyro_client)

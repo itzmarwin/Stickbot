c9a70f958a0264a05336b33085b21d6cbb637d2c
itzmarwin <itzm4rw1n@gmail.com>
2026-06-06 17:49:16 +0530

    Update language.py

--- handlers/language.py
+++ handlers/language.py
@@ -4,8 +4,8 @@ from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from mongo.userdb import get_user_language, set_user_language
-from utils.language import get_text
+from mongo.userdb import set_user_language
+from utils.language import get_lang_dict, get_text_from_dict
 
 logger = logging.getLogger(__name__)
 router = Router()
@@ -22,8 +22,8 @@ def get_language_selection_keyboard():
 
 @router.message(Command("lang"), F.chat.type == "private")
 async def cmd_lang(message: Message):
-    user_id = message.from_user.id
-    lang_select_msg = await get_text(user_id, "LANG_SELECT_MESSAGE")
+    _, lang = await get_lang_dict(message.from_user.id)
+    lang_select_msg = get_text_from_dict(lang, "LANG_SELECT_MESSAGE")
     await message.answer(lang_select_msg, reply_markup=get_language_selection_keyboard())
 
 
@@ -37,7 +37,6 @@ async def set_language_callback(callback: CallbackQuery):
         return
 
     success = await set_user_language(user_id, language)
-
     if not success:
         await callback.answer("Error saving language!", show_alert=True)
         return
@@ -45,19 +44,20 @@ async def set_language_callback(callback: CallbackQuery):
     from handlers.keyboard_utils import get_main_menu_keyboard
     from utils.html_utils import escape_html
 
-    start_text = await get_text(
-        user_id,
+    _, lang = await get_lang_dict(user_id)
+
+    start_text = get_text_from_dict(
+        lang,
         "START_MESSAGE_WITH_IMAGE",
         uid=user_id,
         first_name=escape_html(callback.from_user.first_name)
     )
 
-    keyboard = await get_main_menu_keyboard(user_id)
-    await callback.message.edit_text(start_text, reply_markup=keyboard)
+    await callback.message.edit_text(start_text, reply_markup=get_main_menu_keyboard(lang))
     await callback.answer()
 
 
 async def show_language_selection_for_new_user(message: Message):
-    user_id = message.from_user.id
-    lang_select_msg = await get_text(user_id, "LANG_SELECT_MESSAGE")
+    _, lang = await get_lang_dict(message.from_user.id)
+    lang_select_msg = get_text_from_dict(lang, "LANG_SELECT_MESSAGE")
     await message.answer(lang_select_msg, reply_markup=get_language_selection_keyboard())

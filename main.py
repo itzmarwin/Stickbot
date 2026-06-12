1839a4c3b07673e9485092953d1459deae7eee69
itzmarwin <itzm4rw1n@gmail.com>
2026-06-12 17:04:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ from pyrogram_handlers.mute import setup_mute_handlers
 from pyrogram_handlers.locks import setup_locks_handlers
 from pyrogram_handlers.caching import setup_cache_handlers
 from pyrogram_handlers.antipromo import setup_antipromo_handlers
-from pyrogram_handlers.info import setup_info_handlers
 
 logging.basicConfig(
     level=logging.INFO,
